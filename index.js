884d7b41dad73f23e94b8d1ac1f04d4d5112732c
Md Shahed Chowdhury <shahedchowdhury424@gmail.com>
2025-12-16 22:52:54 +0600

    verify user middleware done

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.port || 3000;
 
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 //middleware
 app.use(express.json());
 app.use(cors());
+
+const verifyFbToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).send({ message: "Unauthorized Access" });
+  }
+  try {
+    const idToken = token.split(" ")[1];
+    if (!idToken) {
+      res.status(401).send({ message: "Unauthorized Access" });
+    }
+    const decoded = admin.auth.verifyFbToken(idToken);
+     req.authorizedEmail = decoded.email;
+    next();
+  } catch {
+    res.status(401).send({ message: "Unauthorized Access" });
+  }
+};
 //mongodb client setup
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://Md-Shahed-Chowdhury:${process.env.DB_PASS}@cluster0.9y5oe7g.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,15 +54,23 @@ async function run() {
     await client.connect();
     const database = client.db("Blood-donation");
     const Users = database.collection("Users");
+    const bloodRequests = database.collection("Blood-request");
 
     //user api
-    app.post("/newUser",async(req,res)=>{
-        const newUser = req.body;
-        newUser.role = "donor";
-        newUser.status = "active";
+    app.post("/newUser", async (req, res) => {
+      const newUser = req.body;
+      newUser.role = "donor";
+      newUser.status = "active";
       const result = await Users.insertOne(newUser);
       res.send(result);
-    })
+    });
+
+    //blood req api
+    app.post("/add-blood-request",verifyFbToken, async (req, res) => {
+      const newReq = req.body;
+      const result = await bloodRequests.insertOne(newReq);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
